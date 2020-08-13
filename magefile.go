42f774c971ac0011df259f5624c10cf280baa133
iwaltgen <iwaltgen@gmail.com>
2020-08-13 23:21:48 +0900

    chore: remove TODO

--- magefile.go
+++ magefile.go
@@ -147,7 +147,6 @@ func Install() error {
 		"github.com/gogo/protobuf/protoc-gen-gogofast",
 		"github.com/gogo/protobuf/protoc-gen-gogofaster",
 		"github.com/gogo/protobuf/protoc-gen-gogoslick",
-		// TODO(iwaltgen): protoc-gen-validate
 		"golang.org/x/tools/cmd/stringer",
 		"github.com/google/wire/cmd/wire",
 		"github.com/rakyll/statik",
